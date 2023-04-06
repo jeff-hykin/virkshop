772749fbfe099014c65dd5ac9d04d106e3e7e7a4
Jeff Hykin <jeff.hykin@gmail.com>
2023-04-06 12:36:38 -0500

    seems to have worked

---
+++ virkshop/events/@setup_after_system_tools/655_555_setup_python_venv.zsh
@@ -0,0 +1,17 @@
+# TMPDIR fixes a pip issue
+export TMPDIR="$VIRKSHOP_FOLDER/temporary.ignore"
+mkdir -p "$TMPDIR"
+export VIRTUAL_ENV="$VIRKSHOP_FOLDER/../.venv"
+export PATH="$HOME/.local/bin:$PATH"
+if ! [ -d "$VIRTUAL_ENV" ]
+then
+    echo "creating virtual env for python"
+    # TODO: run the cleanup
+    . "$VIRKSHOP_FOLDER/settings/extensions/python/during_clean.sh"
+    python -m venv "$VIRTUAL_ENV" && echo "virtual env created"
+fi
+
+export PATH="$VIRTUAL_ENV/bin:$PATH"
+
+# fix SSL issues
+export SSL_CERT_FILE="$(python -c 'import ssl; print(ssl.get_default_verify_paths().openssl_cafile)')"  
