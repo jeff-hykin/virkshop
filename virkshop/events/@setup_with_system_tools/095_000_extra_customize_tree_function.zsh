a3142e2edbb5eea6ae6bcdf9216535c40fda331d
Jeff Hykin <jeff.hykin@gmail.com>
2023-06-07 13:11:20 -0500

    fix tree command

--- virkshop/events/@setup_with_system_tools/095_000_extra_customize_tree_function.zsh
+++ virkshop/events/@setup_with_system_tools/095_000_extra_customize_tree_function.zsh
@@ -1,3 +1,3 @@
 function tree {
-    "$(virkshop_tools nix_path_for tree)/bin/tree" -C --dirsfirst  -A -F --noreport "$@"
+    "$(virkshop_tools nix_path_for package tree)/bin/tree" -C --dirsfirst  -A -F --noreport "$@"
 }
