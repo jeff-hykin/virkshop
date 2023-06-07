dbf9e38a526c5d67a3529f9f9cd3b3a67f013589
Jeff Hykin <jeff.hykin@gmail.com>
2023-06-07 11:57:49 -0500

    update example

--- virkshop/events/@setup_with_system_tools/555_555_custom_example.deno.js
+++ virkshop/events/@setup_with_system_tools/555_555_custom_example.deno.js
@@ -1,4 +1,6 @@
 
 // // uncomment to show the zsh version
+// import { FileSystem } from "https://deno.land/x/quickr@0.6.25/main/file_system.js"
+// import { Console } from "https://deno.land/x/quickr@0.6.25/main/console.js"
 // import { run, returnAsString, Env, Cwd, Stdin, Stdout, Stderr, Out, Overwrite, AppendTo } from "https://deno.land/x/quickr@0.6.25/main/run.js"
 // console.log(await run`zsh --version ${Out(returnAsString)}`)
